521e297ef76e8377bdb0db521039a7e5f47d31b0
DenisaMan <121771420+DenisaMan@users.noreply.github.com>
2023-02-18 22:27:38 +0200

    Add files via upload

---
+++ tema 1/pb1.js
@@ -0,0 +1,21 @@
+const ecuatia1 = 2 + (3 * 4) - 5
+
+console.log( ecuatia1);
+
+const ecuatia2 = 4 *  2 + 6 / 3 
+
+console.log( ecuatia2)
+
+
+const ecuatia3 = 2 * 6 + 10 
+
+console.log ( ecuatia3)
+
+
+const ecuatia4 = ( 5 * ( 3 + 1)) + 3
+
+console.log( ecuatia4)
+
+const ecuatia5 = 10 - 10 + 3 *  5- 2   / 10 + 10
+
+console.log(ecuatia5)
---
+++ tema 1/pb2.js
@@ -0,0 +1,9 @@
+const x = 3
+const y = 1
+
+const e1 = 2 * x -  5 * y
+console.log(e1)
+
+
+const e2 = 3 * x + 9 * y
+console.log(e2)
